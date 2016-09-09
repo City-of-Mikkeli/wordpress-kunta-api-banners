9fd7a7c387b5ba8e781db27917f252ece12cf05e
Antti Leppä <antti.leppa@foyt.fi>
2016-09-09 13:17:07 +0300

    Replaced tabs with whitespaces

--- kunta-api-banners.php
+++ kunta-api-banners.php
@@ -14,28 +14,28 @@ add_action ( 'init', 'kuntaApiBannersCreatePostType' );
 
 function kuntaApiBannersCreatePostType() {
   
-	register_post_type ( 'kunta_api_banners', array (
-	  'labels' => array (
-	    'name'               => __( 'Header Banners', KUNTA_API_BANNERS_I18N_DOMAIN ),
-  	  'singular_name'      => __( 'Header Banner', KUNTA_API_BANNERS_I18N_DOMAIN ),
-	    'add_new'            => __( 'Add Banner', KUNTA_API_BANNERS_I18N_DOMAIN ),
-	    'add_new_item'       => __( 'Add New Banner', KUNTA_API_BANNERS_I18N_DOMAIN ),
-  	  'edit_item'          => __( 'Edit Banner', KUNTA_API_BANNERS_I18N_DOMAIN ),
-	    'new_item'           => __( 'New Banner', KUNTA_API_BANNERS_I18N_DOMAIN ),
-	    'view_item'          => __( 'View Banner', KUNTA_API_BANNERS_I18N_DOMAIN ),
+  register_post_type ( 'kunta_api_banners', array (
+    'labels' => array (
+      'name'               => __( 'Header Banners', KUNTA_API_BANNERS_I18N_DOMAIN ),
+      'singular_name'      => __( 'Header Banner', KUNTA_API_BANNERS_I18N_DOMAIN ),
+      'add_new'            => __( 'Add Banner', KUNTA_API_BANNERS_I18N_DOMAIN ),
+      'add_new_item'       => __( 'Add New Banner', KUNTA_API_BANNERS_I18N_DOMAIN ),
+      'edit_item'          => __( 'Edit Banner', KUNTA_API_BANNERS_I18N_DOMAIN ),
+      'new_item'           => __( 'New Banner', KUNTA_API_BANNERS_I18N_DOMAIN ),
+      'view_item'          => __( 'View Banner', KUNTA_API_BANNERS_I18N_DOMAIN ),
       'search_items'       => __( 'Search Banners', KUNTA_API_BANNERS_I18N_DOMAIN ),
-  	  'not_found'          => __( 'No Banners found', KUNTA_API_BANNERS_I18N_DOMAIN ),
-	    'not_found_in_trash' => __( 'No Banners in trash', KUNTA_API_BANNERS_I18N_DOMAIN ),
-	    'menu_name'          => __( 'Banners', KUNTA_API_BANNERS_I18N_DOMAIN ),
-  	  'all_items'          => __( 'Banners', KUNTA_API_BANNERS_I18N_DOMAIN )
-	  ),
-	  'public' => true,
-	  'has_archive' => true,
-	  'supports' => array (
-	    'title',
-	    'editor',
+      'not_found'          => __( 'No Banners found', KUNTA_API_BANNERS_I18N_DOMAIN ),
+      'not_found_in_trash' => __( 'No Banners in trash', KUNTA_API_BANNERS_I18N_DOMAIN ),
+      'menu_name'          => __( 'Banners', KUNTA_API_BANNERS_I18N_DOMAIN ),
+      'all_items'          => __( 'Banners', KUNTA_API_BANNERS_I18N_DOMAIN )
+    ),
+    'public' => true,
+    'has_archive' => true,
+    'supports' => array (
+      'title',
+      'editor',
       'thumbnail'
- 	  )
+     )
   ));
   
 }
