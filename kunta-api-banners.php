547db9b957728c2295550309183057b78c68cf63
Antti Leppä <antti.leppa@foyt.fi>
2016-09-12 12:36:25 +0300

    Added support for specifying links into banners

--- kunta-api-banners.php
+++ kunta-api-banners.php
@@ -11,10 +11,12 @@ defined ( 'ABSPATH' ) || die ( 'No script kiddies please!' );
 require_once ('constants.php');
 
 add_action ( 'init', 'kuntaApiBannersCreatePostType' );
+add_action ( 'add_meta_boxes', 'kuntaApiBannerMetaBox', 9999, 2 );
+add_action ('save_post', 'kuntaApiBannerSaveLink');
 
 function kuntaApiBannersCreatePostType() {
   
-  register_post_type ( 'kunta_api_banners', array (
+  register_post_type ( 'banner', array (
     'labels' => array (
       'name'               => __( 'Header Banners', KUNTA_API_BANNERS_I18N_DOMAIN ),
       'singular_name'      => __( 'Header Banner', KUNTA_API_BANNERS_I18N_DOMAIN ),
@@ -31,6 +33,7 @@ function kuntaApiBannersCreatePostType() {
     ),
     'public' => true,
     'has_archive' => true,
+    'show_in_rest' => true,
     'supports' => array (
       'title',
       'editor',
@@ -39,5 +42,45 @@ function kuntaApiBannersCreatePostType() {
   ));
   
 }
+
+function kuntaApiBannerRenderMetaBox($banner) {
+  $link = get_post_meta($banner->ID, "banner-link", true);
+  echo '<input name="banner-link" id="banner-link" type="url" style="width: 100%;" value="' . $link . '"></input>';
+}
+
+function kuntaApiBannerMetaBox() {
+  add_meta_box( 
+    'banner-link-meta-box',
+    __( 'Banner Link', KUNTA_API_BANNERS_I18N_DOMAIN ),
+     'kuntaApiBannerRenderMetaBox',
+     'banner',
+     'side',
+     'default'
+  );
+}
+
+function kuntaApiBannerSaveLink($bannerId) {
+  if (array_key_exists('banner-link', $_POST)) {
+	update_post_meta($bannerId, 'banner-link', $_POST['banner-link']);
+  }
+}
+
+function kuntaApiBannerRestGet( $object, $field_name, $request) {
+  return get_post_meta( $object[ 'id' ], $field_name, true);
+}
+
+add_action('rest_api_init', function () {
+	
+  register_rest_field( 'banner', 'banner-link', array(
+	'get_callback' => 'kuntaApiBannerRestGet',
+	'update_callback' => null,
+	'schema' => array (
+	  "type" => "string",
+	  "format" => "url",
+	  "description" => "Banner link"
+	)
+  ));
+  
+});
 
 ?>
